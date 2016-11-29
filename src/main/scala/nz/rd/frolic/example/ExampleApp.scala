2a758f772d593b475976acf501044e5fa0263a97
Rich Dougherty <rich@rd.gen.nz>
2016-11-29 13:42:20 +1300

    Route based on request path

--- src/main/scala/nz/rd/frolic/example/ExampleApp.scala
+++ src/main/scala/nz/rd/frolic/example/ExampleApp.scala
@@ -10,21 +10,33 @@ import nz.rd.frolic.http.{Request, Response}
 object ExampleApp {
   def main(args: Array[String]): Unit = {
     Frolic.start { request: Request =>
-      def threadName(): String = Thread.currentThread.getName
-
-      val buffer = ByteBuffer.allocate(128)
-      val response = new Response {
-        override def statusCode: Int = 200
-        override def send: (SenderTasks) => Task[Unit] = { sender: SenderTasks =>
-          for {
-            _ <- sender.send(s"Hello from thread: ${threadName}. ")
-            bytesRead <- request.entityChannel.read(buffer)
-            _ <- sender.send(s"Read $bytesRead bytes from request channel. ")
-            _ <- sender.send(s"Hello again from thread: ${threadName}.")
-          } yield ()
+      if (request.path == "/plaintext") {
+        val response = new Response {
+          override def statusCode: Int = 200
+          override def send: (SenderTasks) => Task[Unit] = { sender: SenderTasks =>
+            sender.asyncSendAndEnd("Hello world")
+            Task.Value.Unit
+          }
         }
+        Task.Value(response)
+      } else {
+        def threadName(): String = Thread.currentThread.getName
+
+        val buffer = ByteBuffer.allocate(128)
+        val response = new Response {
+          override def statusCode: Int = 200
+
+          override def send: (SenderTasks) => Task[Unit] = { sender: SenderTasks =>
+            for {
+              _ <- sender.send(s"Hello from thread: ${threadName}. ")
+              bytesRead <- request.entityChannel.read(buffer)
+              _ <- sender.send(s"Read $bytesRead bytes from request channel. ")
+              _ <- sender.send(s"Hello again from thread: ${threadName}.")
+            } yield ()
+          }
+        }
+        Task.Value(response)
       }
-      Task.Value(response)
     }
   }
 }
