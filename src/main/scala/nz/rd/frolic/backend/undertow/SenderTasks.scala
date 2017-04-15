a0cf331712cf5170024b025daee699c806da37e4
Rich Dougherty <rich@rd.gen.nz>
2017-04-15 21:25:42 +1200

    Commit partial work

--- src/main/scala/nz/rd/frolic/backend/undertow/SenderTasks.scala
+++ src/main/scala/nz/rd/frolic/backend/undertow/SenderTasks.scala
@@ -7,7 +7,16 @@ class SenderTasks(sender: Sender) {
 //  def send(buffer: ByteBuffer): Task[Unit] = sendTask(sender.send(buffer, _))
 //  def send(buffer: Array[ByteBuffer], callback: IoCallback)
   def send(data: String): Task[Unit] = UndertowBackend.ioTask(sender.send(data, _))
-//  def send(data: String, charset: Charset, callback: IoCallback)
+  def send(writer: Writer[Byte]): Task[Unit] = writer match {
+    case WriteElement(element, f) =>
+      Task.Sequence(UndertowBackend.ioTask(sender.send(Array[Byte](element), _)), f)
+    case WriteChunk(chunk, f) =>
+    case Close(f) =>
+    case Error(cause: Throwable) =>
+
+  }
+
+  //  def send(data: String, charset: Charset, callback: IoCallback)
   def close(): Task[Unit] = UndertowBackend.ioTask(sender.close(_))
 
   def asyncSendAndEnd(data: String): Unit = sender.send(data)
